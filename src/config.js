72425753607d3825a554a8fc33675be2f1ecb289
Aquila-Lopez1 <108690045+Aquila-Lopez1@users.noreply.github.com>
2026-03-04 14:17:40 -0300

    Add configuration constants and settings for Google Sheets application

---
+++ src/config.js
@@ -0,0 +1,11 @@
+// Configuration constants and settings for the Google Sheets application
+
+const CONFIG = {
+    SPREADSHEET_ID: 'your_spreadsheet_id_here',
+    RANGE: 'Sheet1!A1:D10',
+    API_KEY: 'your_api_key_here',
+    CLIENT_ID: 'your_client_id_here',
+    SCOPES: ['https://www.googleapis.com/auth/spreadsheets'],
+};
+
+export default CONFIG;
